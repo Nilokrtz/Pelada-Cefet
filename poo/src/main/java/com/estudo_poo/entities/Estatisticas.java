55377842806df0f844f12442d430f851564fb028
Enrico Corrêa <rockstarfoxy05@gmail.com>
2025-04-07 13:08:52 -0300

    Setters e Getters da classe "Estatísticas"

--- poo/src/main/java/com/estudo_poo/entities/Estatisticas.java
+++ poo/src/main/java/com/estudo_poo/entities/Estatisticas.java
@@ -1,5 +1,70 @@
 package com.estudo_poo.entities;
+public class Estatistica {
+    private int passes;
+    private int escanteio;
+    private int chutes;
+    private int cartoesamarelo;
+    private int cartoesvermelho;
+    private int faltas;
+    private int posse;
+    private String gols;
 
-public class Estatisticas {
-    
+public Estatistica(int passes, int escanteio, int chutes, int cartoesamarelo, int cartoesvermelho, int faltas, int posse, String gols){
+    this.passes=passes;
+    this.escanteio=escanteio;
+    this.chutes=chutes;
+    this.cartoesamarelo=cartoesamarelo;
+    this.cartoesvermelho=cartoesvermelho;
+    this.faltas=faltas;
+    this.posse=posse;
+    this.gols=gols;
+}
+public int setPasses(){
+    return passes;
+}
+public void getPasses(int passes){
+    this.passes=passes;
+}
+public int setEscanteio(){
+    return escanteio;
+}
+public void getEscanteio(int escanteio){
+    this.escanteio=escanteio;
+}
+public int setChutes(){
+    return chutes;
+}
+public void getChutes(int chutes){
+    this.chutes=chutes;
+}
+public int setCartoesamarelo(){
+    return cartoesamarelo;
+}
+public void getCartoesmarelos(int cartoesamarelo){
+    this.cartoesamarelo=cartoesamarelo;
+}
+public int setCartoesvermelho(){
+    return cartoesvermelho;
+}
+public void getCartoesvermelho(int cartoesvermelho){
+    this.cartoesvermelho=cartoesvermelho;
+}
+public int setFaltas(){
+    return faltas;
+}
+public void getFaltas(int faltas){
+    this.faltas=faltas;
+}
+public int setPosse(){
+    return posse;
+}
+public void getPosse(int posse){
+    this.posse=posse;
+}
+public String setGols(){
+    return gols;
+}
+public void getGols(String gols){
+    this.gols=gols;
+}
 }
